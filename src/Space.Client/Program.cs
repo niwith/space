c57799d2ca08ff5b516fa037f8277a51eaea35c2
niwith <7247591+niwith@users.noreply.github.com>
2021-01-15 21:41:09 +1000

    Added very basic loading screen

--- src/Space.Client/Program.cs
+++ src/Space.Client/Program.cs
@@ -19,7 +19,7 @@ namespace Space.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            
             await builder.Build().RunAsync();
             new Class1();
         }
